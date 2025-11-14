8ebf75aa9f7a8ec7d5216fee9b3cc1a88ab49d09
eonseo <eonseo0730@gmail.com>
2025-11-14 22:20:09 +0900

    강사 페이지

---
+++ src/main/java/Instructors/RoundedPanel.java
@@ -0,0 +1,42 @@
+package Instructors;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    int radius;
+    int borderWidth; // 테두리 두께
+    Color borderColor; // 테두리 색
+
+    RoundedPanel(int radius){ // 테두리 없는 경우
+        this.radius=radius;
+        this.borderWidth=0;
+        this.borderColor=null;
+        setOpaque(false); // 배경 투명
+    }
+    RoundedPanel(int radius, int borderWidth, Color borderColor){
+        this.radius=radius;
+        this.borderWidth=borderWidth;
+        this.borderColor=borderColor;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+        Graphics2D g2= (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON); // 계단현상 제거
+
+        super.paintComponent(g2);
+
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0,0,getWidth(),getHeight(),radius,radius);
+
+        if(borderColor!=null) {
+            g2.setColor(borderColor);
+            g2.setStroke(new BasicStroke(borderWidth));
+            g2.drawRoundRect(borderWidth / 2, borderWidth / 2, getWidth() - borderWidth, getHeight() - borderWidth, radius, radius);
+        }
+    }
+
+
+}
