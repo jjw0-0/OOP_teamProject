31659051fb9de5f87d3aa6b4ec3e6a06fe280306
eonseo <eonseo0730@gmail.com>
2025-11-24 17:43:58 +0900

    temp commit

---
+++ src/main/java/com/project/app/repository/UserRepositoryImpl.java
@@ -0,0 +1,11 @@
+package com.project.app.repository;
+
+public class UserRepositoryImpl{
+
+    public boolean checkEnrolled(String userId,String lecId) {
+        return true;
+    }
+
+    public void enrollLecture(String userId, String lecId) {
+    }
+}
